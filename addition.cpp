50793b3b3aefa2964b018516009a025117e0326d
Vijay Andhale <andhalevijay545@gmail.com>
2026-06-09 13:59:37 +0530

    vijay

---
+++ addition.cpp
@@ -0,0 +1,13 @@
+//addition of two numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,b;
+cout<<"Enter First Number: ";
+ cin>>a;
+cout<<"Enter Second number: ";
+cin>>b;
+cout<<(a+b)<<endl;
+return 0;
+}
