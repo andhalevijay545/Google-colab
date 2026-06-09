8af433e25222194dca654f5e2b43304acc0b861d
Vijay Andhale <andhalevijay545@gmail.com>
2026-06-09 14:12:11 +0530

    Add simple subtraction program and binary
    Add a minimal C++ CLI program (subtraction.cpp) that prompts for two integers and prints their difference. The compiled executable (subtraction.exe) is included in the changeset. This is a small demo utility; note the program prints both prompts before reading the inputs.
    
    Signed-off-by: Vijay Andhale <andhalevijay545@gmail.com>

---
+++ subtraction.cpp
@@ -0,0 +1,14 @@
+// subtraction of two numbers
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter a: "<<endl;
+    cout<<"enter b: "<<endl;
+    cin>>a;
+    cin>>b;
+    cout<<(a-b)<<endl;
+    return 0;
+}
+
